d5cff96430d433c76ca45bd0f7f59d9ae9aab2ef
Satvik@3607 <78777667+Satvik3607@users.noreply.github.com>
2026-03-17 09:12:29 +0530

    Time: 0 ms (100%), Space: 27.2 MB (18.78%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.c
@@ -0,0 +1,23 @@
+void reverse(int* nums, int start, int end) {
+    while(start < end) {
+        int temp = nums[start];
+        nums[start] = nums[end];
+        nums[end] = temp;
+        start++;
+        end--;
+    }
+}
+
+void rotate(int* nums, int numsSize, int k) {
+
+    k = k % numsSize; // important ⚠️
+
+    // Step 1: reverse whole array
+    reverse(nums, 0, numsSize - 1);
+
+    // Step 2: reverse first k elements
+    reverse(nums, 0, k - 1);
+
+    // Step 3: reverse remaining
+    reverse(nums, k, numsSize - 1);
+}
